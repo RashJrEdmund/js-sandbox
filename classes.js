b13403bf69441c401f8b281cee8601743c3440d1
rash <orashusedmund@gmail.com>
2023-04-24 10:48:55 +0100

    working on classes

---
+++ classes.js
@@ -0,0 +1,27 @@
+const Locomotive = require('./objects');
+// const locomotive = new Locomotive()
+
+// console.log('this is locomotive', locomotive.customSort([1, 2, 4, 3, 1, 0, -1, -10, -2, -3]));
+class MyClass {
+  constructor() {
+    this.locomotive = new Locomotive();
+  }
+
+  logMessage(message) {
+    console.log('this is message:', message);
+  }
+}
+
+class SecondClass {
+  constructor() {
+    this.myClass = new MyClass();
+  }
+
+  write(message) {
+    this.myClass.logMessage(message);
+  }
+}
+
+const secondClass = new SecondClass();
+
+secondClass.write('here i am')
--- objects.js
+++ objects.js
@@ -6,13 +6,20 @@ const Cars = function(nom, pistons, max_gear, brand) {
 }
 
 class Locomotive {
-  
+  customSort(arr) {
+    return arr.sort((a, z) => a - z);
+  }
 }
+
+const custom = new Locomotive();
+custom.customSort([2, 3, 1, 3, 4]);
 
 const myFirstCar = new Cars('Ford Mustang', 8, 6, 'Ford');
 const mySecondCar = new Cars('Mucieliago', 8, 6, 'Lamborghini');
 
 Cars.prototype.wheels = 4;
 
-console.log(myFirstCar);
-console.log(mySecondCar);
+// console.log(myFirstCar);
+// console.log(mySecondCar);
+
+module.exports = Locomotive
