ef5fa2a82b5d430e86d69661a6c1e24c6fc594d8
rash <orashusedmund@gmail.com>
2024-02-08 23:57:19 +0100

    testing on drop

--- src/constants/nav-constants.js
+++ src/constants/nav-constants.js
@@ -1,8 +1,8 @@
 const PATHS = [
+  { folder_name: 'login', name: 'login form' },
   { folder_name: 'slideshow', name: 'slide show' },
   { folder_name: 'color-picker', name: 'color picker' },
   { folder_name: 'rangeSelector', name: 'range selector' },
-  { folder_name: 'login', name: 'login form' },
   { folder_name: 'intersection-observer', name: 'intersection observer' },
   { folder_name: 'drag-and-drop', name: 'drag and drop' }
 ];
--- src/pages/drag-and-drop/index.js
+++ src/pages/drag-and-drop/index.js
@@ -1,3 +1,28 @@
 import { createNavBar } from '../../components/index.js'
 
 createNavBar('drag and drop');
+
+const containers = document.querySelectorAll('.container');
+const draggables = document.querySelectorAll('.draggable');
+
+draggables.forEach((draggable) => {
+  draggable.addEventListener('dragstart', () => {
+    draggable.classList.add('dragging'); // to show what was beign dragged
+  });
+
+  draggable.addEventListener('dragend', () => {
+    draggable.classList.remove('dragging'); //
+  });
+});
+
+containers.forEach((container) => {
+  container.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    console.clear();
+    console.log('dragging over', e);
+  });
+
+  container.addEventListener('drop', (e) => {
+    e.preventDefault();
+  })
+});
