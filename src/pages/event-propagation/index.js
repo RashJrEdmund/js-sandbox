8ff0fc93513963f4a22dd3c96b82f5ee35a1db6d
rash <orashusedmund@gmail.com>
2024-02-22 02:14:45 +0100

    done with propagation

--- src/pages/event-propagation/index.js
+++ src/pages/event-propagation/index.js
@@ -1,46 +1,36 @@
 import { createNavBar } from '../../components/index.js'
 createNavBar('event propagation');
 
-const displayMessage = (message) => {
-  const messageContainer = document.querySelector('.message > span');
-  messageContainer.innerHTML = message;
-  messageContainer.classList.add('flash');
-
-  setTimeout(() => {
-    messageContainer.classList.remove('flash');
-    messageContainer.innerHTML = 'messages for propagation';
-  }, 100);
-}
-
-const animateMessage = (message) => {
-  const timeId = setTimeout(() => {
-    displayMessage(message);
-  }, 500); // 0.5s
-};
-
 const container1 = document.querySelector('.container1');
-const stopBtn1 = document.querySelector('.container1 > button');
 
 const container2 = document.querySelector('.container2');
-const stopBtn2 = document.querySelector('.container2 > button');
 
 const container3 = document.querySelector('.container3');
-const stopBtn3 = document.querySelector('.container3 > button');
 
-stopBtn1.addEventListener('click', () => {
-  displayMessage('clicked me');
-});
+// CONTAINER 1
+container1.addEventListener('click', (e) => {
+  console.log('captured 1');
+}, true); // meaning capture
 
-container1.addEventListener('click', () => {
-  animateMessage('click 1');
-}, true); // meaning no capture hence bubble.
+container1.addEventListener('click', (e) => {
+  console.log('bubbled 1');
+}, false); // meaning no capture hence bubble.
 
 // CONTAINER 2
-container2.addEventListener('click', () => {
-  animateMessage('click 2');
+container2.addEventListener('click', (e) => {
+  // e.stopPropagation();
+  console.log('captured 2');
 }, true);
 
+container2.addEventListener('click', (e) => {
+  console.log('bubbled 2');
+}, false);
+
 // CONTAINER 3
-container3.addEventListener('click', () => {
-  animateMessage('click 3');
+container3.addEventListener('click', (e) => {
+  console.log('captured 3');
 }, true);
+
+container3.addEventListener('click', (e) => {
+  console.log('bubbled 3');
+}, false);
