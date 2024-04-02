9d21d6c2d6d6c56c3e859178f4ec1737272e7d70
rash <orashusedmund@gmail.com>
2024-04-02 18:23:17 +0100

    done simple bubble sort

--- sorting algorithms/bubble-sort.js
+++ sorting algorithms/bubble-sort.js
@@ -1,3 +1,21 @@
 const bubbleSort = (arr) => {
   const n = arr.length;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (i === j) continue;
+
+      if (arr[i] < arr[j]) { // doing < works buh I honestly do not get why < instead of >.
+        const temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+      }
+    }
+  }
+
+  return arr;
 };
+
+const arrToSort = [3, 4, 1, 2, 5];
+
+console.log(bubbleSort([...arrToSort]));
